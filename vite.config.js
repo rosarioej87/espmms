19b60f4a4eb9ad78980cac8191ea9a0127572777
Emil June Rosario <rosarioej87@gmail.com>
2023-11-09 00:21:53 +0800

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import laravel, { refreshPaths } from 'laravel-vite-plugin';
+import {resolve} from 'path'
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
+            refresh: [
+                'app/Http/Livewire/**',
+            ],
+        }),
+    ]
+});
